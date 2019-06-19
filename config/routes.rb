f1962bcd79f3e2ef41f21c3a194cc94eaa731800
kascata <fabiojd47@gmail.com>
2019-06-18 23:55:32 -0300

    Application generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lists
   get 'list/index'
 
   root 'list#index'
---
+++ db/migrate/20190619023900_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration
+  def change
+    create_table :lists do |t|
+      t.string :title
+      t.string :description
+      t.boolean :bought
+
+      t.timestamps null: false
+    end
+  end
+end
