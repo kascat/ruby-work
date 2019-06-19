d8eced252199bba2ea147b5878ca17b3a5fd5d19
kascata <fabiojd47@gmail.com>
2019-06-19 02:12:19 -0300

    validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ActiveRecord::Base
-    has_many :items
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :items
+  has_many :items, dependent: :destroy
+  accepts_nested_attributes_for :items, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
 end
